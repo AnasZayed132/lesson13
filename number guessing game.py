a10a853e15c8896932841c8a9620fa50e4c5c046
AnasZayed132 <trainhiit007@gmail.com>
2026-08-26 15:00:43 +0200

    Add files via upload

---
+++ number guessing game.py
@@ -0,0 +1,30 @@
+print("=====Guess The Number from 1-50=====")
+
+
+
+player_guess=int(input("guess the nunmber from 1-50 you have 5 hearts left"))
+
+secret=
+
+if player_guess == "false":
+  player_guess=int(input("you got it wrong you have 4 hearts left guess the nunmber from 1-50 again "))
+elif player_guess =="true":
+ print("you got the answer right")
+elif player_guess == "false":
+  player_guess=int(input("you got it wrong you have 3 hearts left guess the nunmber from 1-50 again "))
+elif player_guess =="true":
+ print("you got the answer right")
+elif player_guess == "false":
+  player_guess=int(input("you got it wrong you have 3 hearts left guess the nunmber from 1-50 again "))
+elif player_guess =="true":
+ print("you got the answer right")
+elif player_guess == "false":
+  player_guess=int(input("you got it wrong you have 2 hearts left guess the nunmber from 1-50 again "))
+elif player_guess =="true":
+ print("you got the answer right")
+elif player_guess == "false":
+  player_guess=int(input("you got it wrong you have 1 hearts left guess the nunmber from 1-50 again "))
+elif player_guess =="true":
+ print("you got the answer right")
+elif player_guess == "false":
+  print("you have lost the guess was ", )
