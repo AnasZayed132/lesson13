75546464a34276f8c1a17b4a6ff2d4dc4fbcc611
AnasZayed132 <trainhiit007@gmail.com>
2026-09-19 12:18:40 +0200

    Add files via upload

--- number guessing game.py
+++ number guessing game.py
@@ -1,30 +1,55 @@
-print("=====Guess The Number from 1-50=====")
+# game settings
+secret=39
+max_attempts=5
+# setup
+count=0
+guess=0
+print("=" * 42)
+print("I have a secret number from 1 to 50 guess it and win the game dont and stay a loser for the rest of your life. \n After each bad guess i will tell you if your guess is hot or cold now let the games start.")
+print("=" * 42)
+# main game loop
+while count < max_attempts and guess != secret:
+ 
+ guess = int(input("Your guess:"))
+ count=count+1
+ if guess ==secret:
+  print("\n Correct! You guessed it in", count, "attempt(s)!")
+  print("finally i was worried you left ")
+# diff caluculators
+ else:
+  if guess>secret:
+   diff = guess-secret
+  else:
+   diff = secret-guess
+# hint levels
+ if diff>=20:
+  print("yo this is freezing your not good at this")
+ elif diff>=10:
+  print("cold but still you are not doing good")
+ elif diff>=1:
+  print("warm not that bad still not there")
+ else:
+  print("you did it woohoo ladida its not much of an achievment ")
 
+ 
+# remaining lives
+remaining=max_attempts - count 
+if max_attempts==0:
+ print("your a looser you had 5 chances bro!")
+elif max_attempts == 1:
+ print("you have one last try come on i dont believe in you")
+elif max_attempts == 2:
+ print("you have 2 tries left carefull.")
+elif max_attempts == 3:
+ print("3 tries left okay!")
+elif max_attempts == 4:
+ print("4 tries left come on man")
+elif max_attempts == 5:
+ print("5 tries left carefull you have a chance")
 
-
-player_guess=int(input("guess the nunmber from 1-50 you have 5 hearts left"))
-
-secret=
-
-if player_guess == "false":
-  player_guess=int(input("you got it wrong you have 4 hearts left guess the nunmber from 1-50 again "))
-elif player_guess =="true":
- print("you got the answer right")
-elif player_guess == "false":
-  player_guess=int(input("you got it wrong you have 3 hearts left guess the nunmber from 1-50 again "))
-elif player_guess =="true":
- print("you got the answer right")
-elif player_guess == "false":
-  player_guess=int(input("you got it wrong you have 3 hearts left guess the nunmber from 1-50 again "))
-elif player_guess =="true":
- print("you got the answer right")
-elif player_guess == "false":
-  player_guess=int(input("you got it wrong you have 2 hearts left guess the nunmber from 1-50 again "))
-elif player_guess =="true":
- print("you got the answer right")
-elif player_guess == "false":
-  player_guess=int(input("you got it wrong you have 1 hearts left guess the nunmber from 1-50 again "))
-elif player_guess =="true":
- print("you got the answer right")
-elif player_guess == "false":
-  print("you have lost the guess was ", )
+# game over check
+if guess != secret:
+ print("="*5)
+ print("you lost game over your chances went bye bye. \n the secret number was ",secret)
+ print("The annoyed talk was for experimental purposes only.")
+ print("="*5)
